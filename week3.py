993a20ae4da4bedcf69688c68238f79a8355b45d
Salgado <msalgado85@cps.k12.il.us>
2024-09-11 11:50:51 -0500

    first commit

---
+++ week3.py
@@ -0,0 +1,244 @@
+# week 3
+# # Week3
+# # This week we will work on :
+# # Working With Strings
+
+
+# # 1.   Working With Numbers
+# # 2.   Getting Input From Users
+
+
+
+
+# # 1.   Building a Basic Calculator
+# # 2.   Mad Libs Game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Review
+# create variables for the following :
+# 1. age
+# 2. name
+# 3. song
+# 4. food
+# 5. number
+age = 17
+name = "manny"
+song = "yaminabe"
+food = "rice"
+number = 777
+# #now include the variables you just made print in the following...
+
+
+# Once upon a time, there was a [age] old coder named [name].
+
+print("Once upon a time, there was a " + str(age) + " old coder " + name)
+
+# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+
+print(name + " liked to hum the song, " + song + "while coding. It was so annoying that their teammates would throw " + food + "until " + name + " would stop singing." )
+
+# Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+
+print("still, " + name + "was the best coder on the team and could write, " + str(number) + " lines of code every day. Maybe " + song + "was  " + name + "'s secret power?")
+##########################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################
+# The names you use when creating these labels need to follow a few rules:
+# 1. Names can not start with a number.
+# 2. There can be no spaces in the name, use _ instead.
+# 3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
+# 4. It's considered best practice (PEP8) that names are lowercase.
+# 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh),
+#    or 'I' (uppercase letter eye) as single character variable names.
+# 6. Avoid using words that have special meaning in Python like "list" and "str"
+
+
+# Here are some exercises to practice the rules:
+
+
+# Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
+
+
+# 1st_name --> first_name
+# last name --> last_name
+# email@address -->email_address
+# percent% --> percent
+# variable#name --> variable_name
+# O --> zero
+# list --> name
+# Creating Valid Names: Create valid names for the following descriptions:
+
+
+# The first name of a person --> firstName
+# The last name of a person --> last_Name
+# The email address of a person --> email_address
+# The percentage of marks obtained by a student --> percent
+# A variable to store the number of items in a shopping cart --> number_items
+
+
+
+
+# Identify Valid and Invalid Names: Identify which of the following names are valid or invalid according to the rules:
+
+
+# first_name --> valid
+# lastName --> valid
+# email_address --> valid
+# percentage --> valid
+# variable_name --> valid
+# 1_variable --> invalid
+# email@address --> invalid
+# percentage% --> invalid
+# i --? valid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################################################################
+
+
+# # **Working with** **numbers** **bold text**
+# We'll learn about the following topics:
+# 1. Types of Numbers in Python
+# 2. Basic Arithmetic
+# 3. Differences between classic division and floor division
+
+
+# Python has various "types" of numbers (numeric literals).
+# 1. We'll mainly focus on integers and floating point numbers.
+# Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
+# 2. Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
+
+
+##########################################################################################
+# #addition 
+7 + 7
+# #multiplication
+7 * 7
+# #division
+7 / 7
+# #modulo
+# #powers
+7^7
+# #get the max and min of a number
+# #round a number
+# # absolute value
+# # order of operations
+# #to do more you need to import special math libraries from python
+# #from math import *    
+# #this goes out and grabs some different math functions we can use
+# #floor method
+# #ceil method
+# #sqrt method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################################################
+# So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
+# # **Getting Input from users**
+# #how do we get input from users?
+# input("what is your name?")
+# # basic math calculator
+# #ask the user for 2 numbers
+# # print out a statement where you:
+# # add them together
+# #multiply
+# # find the max number
+# # find the remainder of the numbers
+# #round one number
+
+
+
+
+
+
+
+
+##########################################################################################
+# # mad libs game
+# print("Roses are {color}")
+# print("{plural noun} are blue")
+# print("I love {celebrity}")
+# # On to codehs.com
+
+
+
+
+
+
+
+
